3116bdf382dddb900016f50616b965b094eca728
pupil <pupil@ivkhk.ee>
2024-05-21 13:02:48 +0300

    db

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize('animalshelter', 'root', '', {
-    host: 'localhost',
+const db = new Sequelize('niu2syjparlb3opj', 'apb2ekiqnhsbwork', 'ko0jtqpa4yo5wlt4', {
+    host: 'zpj83vpaccjer3ah.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
     dialect: 'mysql',
 });
 
